42856ff9c2976d32cbda56b7da12136ccbe2438e
Thom <thomvanoorschot@hotmail.com>
2025-05-31 19:08:22 +0200

    Simplified way of consuming library. Can only handle emscripten for now

--- build.zig
+++ build.zig
@@ -1,22 +1,6 @@
 const std = @import("std");
 const Build = std.Build;
 
-pub const Backend = enum {
-    no_backend,
-    glfw_wgpu,
-    glfw_opengl3,
-    glfw_vulkan,
-    glfw_dx12,
-    win32_dx12,
-    glfw,
-    sdl2_opengl3,
-    osx_metal,
-    sdl2,
-    sdl3,
-    sdl3_gpu,
-    sdl3_opengl3,
-};
-
 const cflags = &.{
     "-fno-sanitize=undefined",
     "-Wno-elaborated-enum-base",
@@ -27,32 +11,67 @@ pub const IMGUI_C_DEFINES: []const [2][]const u8 = &.{
     .{ "IMGUI_IMPL_API", "extern \"C\"" },
 };
 
-const Options = struct {
-    backend: Backend,
-    with_imgui: bool,
-    with_implot: bool,
-};
+pub var defaultWithImgui: bool = true;
+pub var defaultWithImplot: bool = true;
+pub var defaultUseGLFW: bool = false;
+pub var defaultUseWebGPU: bool = false;
+pub var defaultUseFilesystem: bool = true;
+pub var defaultTarget: Build.ResolvedTarget = undefined;
+pub var defaultBrowser: []const u8 = "chrome";
+pub var defaultCpuArg: std.Target.Cpu.Arch = .wasm64;
+pub var defaultOptimize: std.builtin.OptimizeMode = .ReleaseSmall;
+pub var defaultReleaseUseLTO: bool = true;
+pub var defaultReleaseUseClosure: bool = true;
 
-pub fn build(
-    b: *Build,
-) void {
+pub fn build(b: *Build) !void {
     var target_query = b.standardTargetOptionsQueryOnly(.{});
     if (target_query.os_tag == .emscripten) {
         target_query.cpu_features_add = std.Target.wasm.featureSet(&.{
             .atomics,
             .bulk_memory,
         });
+    } else {
+        return error.UnsupportedOS;
     }
     const target = b.resolveTargetQuery(target_query);
-
     const optimize = b.standardOptimizeOption(.{});
 
+    const with_imgui: bool =
+        b.option(bool, "with_imgui", "Enable ImGui support") orelse true;
+    const with_implot: bool =
+        b.option(bool, "with_implot", "Enable ImPlot support") orelse true;
+
+    const use_glfw: bool =
+        b.option(bool, "use_glfw", "Enable GLFW in Emscripten") orelse false;
+    const use_webgpu: bool =
+        b.option(bool, "use_webgpu", "Enable WebGPU in Emscripten") orelse false;
+    const use_filesystem: bool =
+        b.option(bool, "use_filesystem", "Enable file system support in Emscripten") orelse true;
+    const browser: []const u8 =
+        b.option([]const u8, "browser", "Browser to use for emrun") orelse "chrome";
+    const release_use_lto: bool =
+        b.option(bool, "release_use_lto", "Enable LTO in release builds") orelse true;
+    const release_use_closure: bool =
+        b.option(bool, "release_use_closure", "Enable Closure compiler in release builds") orelse true;
+
+    defaultWithImgui = with_imgui;
+    defaultWithImplot = with_implot;
+    defaultUseGLFW = use_glfw;
+    defaultUseWebGPU = use_webgpu;
+    defaultUseFilesystem = use_filesystem;
+    defaultBrowser = browser;
+    defaultOptimize = optimize;
+    defaultTarget = target;
+    defaultReleaseUseLTO = release_use_lto;
+    defaultReleaseUseClosure = release_use_closure;
+    if (target_query.cpu_arch != null) {
+        defaultCpuArg = target_query.cpu_arch.?;
+    }
     const zignite_mod = b.addModule("zignite", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     const zignite = b.addLibrary(.{
         .name = "zignite",
         .root_module = zignite_mod,
@@ -63,42 +82,24 @@ pub fn build(
     zignite.linkLibC();
 
     const emsdk = b.dependency("emsdk", .{});
-
-    const maybe_activate_emsdk = try ensureEmsdkInstalled(
-        b,
-        emsdk,
-    );
-    if (maybe_activate_emsdk) |activateStep| {
+    if (emSdkSetupStep(b, emsdk)) |activateStep| {
         zignite.step.dependOn(&activateStep.step);
-        const embuilder_path = emSdkLazyPath(
-            b,
-            emsdk,
-            &.{ "upstream", "emscripten", "embuilder" },
-        ).getPath(b);
-
+        const embuilder_path = emSdkLazyPath(b, emsdk, &.{ "upstream", "emscripten", "embuilder" }).getPath(b);
         const embuilder = b.addSystemCommand(&.{embuilder_path});
         embuilder.step.dependOn(&activateStep.step);
-        embuilder.setCwd(emsdk.path("."));
 
-        // Set all the necessary emsdk environment variables
         const emsdk_root = emsdk.path(".").getPath(b);
         embuilder.setEnvironmentVariable("EMSDK", emsdk_root);
         embuilder.setEnvironmentVariable("EMSCRIPTEN", b.pathJoin(&.{ emsdk_root, "upstream", "emscripten" }));
         embuilder.setEnvironmentVariable("EM_CONFIG", b.pathJoin(&.{ emsdk_root, ".emscripten" }));
         embuilder.setEnvironmentVariable("EM_CACHE", b.pathJoin(&.{ emsdk_root, "upstream", "emscripten", "cache" }));
-
         embuilder.addArgs(&.{ "build", "contrib.glfw3" });
         zignite.step.dependOn(&embuilder.step);
     }
     const emsdk_include_path = emsdk.path(b.pathJoin(&.{ "upstream", "emscripten", "cache", "sysroot", "include" }));
     zignite.addSystemIncludePath(emsdk_include_path);
 
-    const options = Options{
-        .backend = b.option(Backend, "backend", "") orelse .glfw_wgpu,
-        .with_imgui = b.option(bool, "with_imgui", "") orelse true,
-        .with_implot = b.option(bool, "with_implot", "") orelse true,
-    };
-    if (options.with_imgui) {
+    if (with_imgui) {
         zignite.addIncludePath(b.path("libs/imgui"));
         zignite.addIncludePath(b.path("libs/imgui/backends"));
         for (IMGUI_C_DEFINES) |c_define| {
@@ -108,7 +109,6 @@ pub fn build(
             .files = &.{
                 "libs/cimgui.cpp",
                 "libs/cimgui_impl.cpp",
-
                 "libs/imgui/imgui.cpp",
                 "libs/imgui/imgui_widgets.cpp",
                 "libs/imgui/imgui_tables.cpp",
@@ -117,14 +117,21 @@ pub fn build(
             },
             .flags = cflags,
         });
+        zignite.addCSourceFile(.{
+            .file = b.path("libs/imgui/backends/imgui_impl_glfw.cpp"),
+            .flags = cflags,
+        });
+        zignite.addCSourceFile(.{
+            .file = b.path("libs/imgui/backends/imgui_impl_wgpu.cpp"),
+            .flags = cflags,
+        });
     }
 
-    if (options.with_implot) {
+    if (with_implot) {
         zignite.addIncludePath(b.path("libs/implot"));
         zignite.addCSourceFiles(.{
             .files = &.{
                 "libs/cimplot.cpp",
-
                 "libs/implot/implot_demo.cpp",
                 "libs/implot/implot.cpp",
                 "libs/implot/implot_items.cpp",
@@ -133,50 +140,16 @@ pub fn build(
         });
     }
 
-    const isEmscripten = target.result.os.tag == .emscripten;
-    switch (options.backend) {
-        .glfw_wgpu => {
-            if (!isEmscripten) {
-                // zignite.addIncludePath(.{ .cwd_relative = "libs/wgpu" });
-
-                // zignite.addCSourceFile(.{ .file = b.path("libs/wgpu/lib_webgpu_cpp20.cpp"), .flags = &.{""} });
-                // zignite.addCSourceFile(.{ .file = b.path("libs/wgpu/lib_webgpu.cpp"), .flags = &.{""} });
-            }
-
-            if (options.with_imgui) {
-                zignite.addCSourceFile(.{
-                    .file = b.path("libs/imgui/backends/imgui_impl_glfw.cpp"),
-                    .flags = cflags,
-                });
-                zignite.addCSourceFile(.{
-                    .file = b.path("libs/imgui/backends/imgui_impl_wgpu.cpp"),
-                    .flags = cflags,
-                });
-            }
-        },
-        else => unreachable,
-    }
     b.installArtifact(zignite);
 }
 
-fn emSdkSetupStep(b: *Build, emsdk: *Build.Dependency) !?*Build.Step.Run {
+fn emSdkSetupStep(b: *Build, emsdk: *Build.Dependency) ?*Build.Step.Run {
     const emsdk_install = createEmsdkStep(b, emsdk);
     emsdk_install.addArgs(&.{ "install", "latest" });
     const emsdk_activate = createEmsdkStep(b, emsdk);
     emsdk_activate.addArgs(&.{ "activate", "latest" });
     emsdk_activate.step.dependOn(&emsdk_install.step);
     return emsdk_activate;
-}
-
-// -------------------------------------------------------------------
-// (B) ‒ New public helper that just forwards to emSdkSetupStep.
-//     Downstream projects will call this directly.
-// -------------------------------------------------------------------
-pub fn ensureEmsdkInstalled(
-    b: *Build,
-    emsdk: *Build.Dependency,
-) !?*Build.Step.Run {
-    return emSdkSetupStep(b, emsdk);
 }
 
 fn createEmsdkStep(b: *Build, emsdk: *Build.Dependency) *Build.Step.Run {
@@ -189,106 +162,13 @@ fn emSdkLazyPath(b: *Build, emsdk: *Build.Dependency, subPaths: []const []const 
     return emsdk.path(b.pathJoin(subPaths));
 }
 
-pub const EmLinkOptions = struct {
-    emsdk: *Build.Dependency,
-    shell_file_path: ?Build.LazyPath,
-    b_options: EmOptions,
-};
-pub fn emLinkStep(
-    b: *Build,
-    l_options: EmLinkOptions,
-) !*Build.Step.InstallDir {
-    const emcc_path = emSdkLazyPath(b, l_options.emsdk, &.{ "upstream", "emscripten", "emcc" }).getPath(b);
-    const emcc = b.addSystemCommand(&.{emcc_path});
-    emcc.setName("emcc"); // hide emcc path
-
-    if (l_options.b_options.optimize == .Debug) {
-        emcc.addArgs(&.{ "-Og", "-sSAFE_HEAP=1", "-sSTACK_OVERFLOW_CHECK=1", "-sASSERTIONS=1" });
-    } else {
-        emcc.addArg("-sASSERTIONS=0");
-        if (l_options.b_options.optimize == .ReleaseSmall) {
-            emcc.addArg("-Oz");
-        } else {
-            emcc.addArg("-O3");
-        }
-        if (l_options.b_options.release_use_lto) {
-            emcc.addArg("-flto");
-        }
-        if (l_options.b_options.release_use_closure) {
-            emcc.addArgs(&.{ "--closure", "1" });
-        }
-    }
-
-    emcc.addArg("-pthread");
-    emcc.addArg(b.fmt("-sPTHREAD_POOL_SIZE={d}", .{1}));
-
-    emcc.addArg("-sERROR_ON_UNDEFINED_SYMBOLS=0");
-    emcc.addArg("-sALLOW_MEMORY_GROWTH=1");
-    emcc.addArg("-sASYNCIFY");
-    emcc.addArg("-sMEMORY64=1");
-    if (l_options.b_options.use_webgpu) {
-        emcc.addArg("-sUSE_WEBGPU=1");
-    }
-    if (l_options.b_options.use_glfw) {
-        emcc.addArg("--use-port=contrib.glfw3");
-    }
-    if (!l_options.b_options.use_filesystem) {
-        emcc.addArg("-sNO_FILESYSTEM=1");
-    }
-    if (l_options.shell_file_path) |shell_file_path| {
-        emcc.addPrefixedFileArg("--shell-file=", shell_file_path);
-    }
-    for (l_options.b_options.extra_args) |arg| {
-        emcc.addArg(arg);
-    }
-
-    emcc.addArtifactArg(l_options.b_options.lib_main);
-    for (l_options.b_options.lib_main.getCompileDependencies(false)) |item| {
-        if (item.kind == .lib) {
-            emcc.addArtifactArg(item);
-        }
-    }
-    if (l_options.b_options.optimize == .Debug or l_options.b_options.optimize == .ReleaseSafe) {
-        emcc.addArgs(&[_][]const u8{
-            "-sUSE_OFFSET_CONVERTER",
-            "-sASSERTIONS",
-        });
-    }
-
-    emcc.addArg("-o");
-    const out_file = emcc.addOutputFileArg(b.fmt("{s}.html", .{l_options.b_options.lib_main.name}));
-
-    const install = b.addInstallDirectory(.{
-        .source_dir = out_file.dirname(),
-        .install_dir = .prefix,
-        .install_subdir = "web",
-    });
-    install.step.dependOn(&emcc.step);
-    return install;
-}
-
-pub const EmOptions = struct {
-    target: Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-    zignite_dep: *Build.Dependency,
+pub fn emRunStep(b: *Build, args: struct {
     name: []const u8,
-    browser: ?[]const u8 = "chrome",
+    zignite_dep: *Build.Dependency,
     lib_main: *Build.Step.Compile,
-    release_use_closure: bool = true,
-    release_use_lto: bool = true,
-    use_webgpu: bool = false,
-    use_glfw: bool = false,
-    use_filesystem: bool = true,
-    extra_args: []const []const u8 = &.{},
-};
-pub fn emRunStep(b: *Build, options: EmOptions) !void {
-    const emsdk = options.zignite_dep.builder.dependency("emsdk", .{});
-
-    const link_step = try emLinkStep(b, .{
-        .b_options = options,
-        .emsdk = emsdk,
-        .shell_file_path = options.zignite_dep.path("web/shell.html"),
-    });
+}) void {
+    const emsdk = args.zignite_dep.builder.dependency("emsdk", .{});
+    const link_step = emLinkStep(b, args.zignite_dep, args.lib_main);
 
     b.getInstallStep().dependOn(&link_step.step);
 
@@ -299,13 +179,71 @@ pub fn emRunStep(b: *Build, options: EmOptions) !void {
     ).getPath(b);
 
     const emrun = b.addSystemCommand(&.{emrun_path});
-
-    if (options.browser) |browser| {
-        emrun.addArgs(&.{ "--browser", browser });
-    }
-
-    emrun.addArg(b.fmt("{s}/web/{s}.html", .{ b.install_path, options.name }));
+    emrun.addArgs(&.{ "--browser", defaultBrowser });
+    // (Here you could also pull “defaultBrowser” from a b.option, if desired.)
+    emrun.addArg(b.fmt("{s}/web/{s}.html", .{ b.install_path, args.name }));
     emrun.step.dependOn(&link_step.step);
 
-    b.step(options.name, "").dependOn(&emrun.step);
+    return b.step(args.name, "").dependOn(&emrun.step);
+}
+
+pub fn emLinkStep(b: *Build, zignite_dep: *Build.Dependency, lib_main: *Build.Step.Compile) *Build.Step.InstallDir {
+    const emsdk = zignite_dep.builder.dependency("emsdk", .{});
+    const emcc_path = emSdkLazyPath(b, emsdk, &.{ "upstream", "emscripten", "emcc" }).getPath(b);
+    const emcc = b.addSystemCommand(&.{emcc_path});
+    emcc.setName("emcc");
+
+    if (defaultOptimize == .Debug) {
+        emcc.addArgs(&.{ "-Og", "-sSAFE_HEAP=1", "-sSTACK_OVERFLOW_CHECK=1", "-sASSERTIONS=1" });
+    } else {
+        emcc.addArg("-sASSERTIONS=0");
+        if (defaultOptimize == .ReleaseSmall) {
+            emcc.addArg("-Oz");
+        } else {
+            emcc.addArg("-O3");
+        }
+        if (defaultReleaseUseLTO) {
+            emcc.addArg("-flto");
+        }
+        if (defaultReleaseUseClosure) {
+            emcc.addArgs(&.{ "--closure", "1" });
+        }
+    }
+
+    emcc.addArg("-pthread");
+    emcc.addArg(b.fmt("-sPTHREAD_POOL_SIZE={d}", .{1}));
+    emcc.addArg("-sERROR_ON_UNDEFINED_SYMBOLS=0");
+    emcc.addArg("-sALLOW_MEMORY_GROWTH=1");
+    emcc.addArg("-sASYNCIFY");
+
+    if (defaultCpuArg == .wasm64) {
+        emcc.addArg("-sMEMORY64=1");
+    }
+    if (defaultUseWebGPU) {
+        emcc.addArg("-sUSE_WEBGPU=1");
+    }
+    if (defaultUseGLFW) {
+        emcc.addArg("--use-port=contrib.glfw3");
+    }
+    if (!defaultUseFilesystem) {
+        emcc.addArg("-sNO_FILESYSTEM=1");
+    }
+
+    emcc.addArtifactArg(lib_main);
+    for (lib_main.getCompileDependencies(false)) |item| {
+        if (item.kind == .lib) {
+            emcc.addArtifactArg(item);
+        }
+    }
+
+    emcc.addArg("-o");
+    const out_file = emcc.addOutputFileArg(b.fmt("{s}.html", .{"zignite"}));
+
+    const install = b.addInstallDirectory(.{
+        .source_dir = out_file.dirname(),
+        .install_dir = .prefix,
+        .install_subdir = "web",
+    });
+    install.step.dependOn(&emcc.step);
+    return install;
 }
